be2effd1ac632d8b51913d746ba964cc7da06dbd
Sacha Lhopital <sacha.lhopital@etu.univ-lyon1.fr>
2018-05-02 16:57:18 +0200

    Fix issue in deployment script

--- deployment_script.sh
+++ deployment_script.sh
@@ -2,5 +2,5 @@
 nmap -sn 10.33.120.1-255
 
 # Find all Raspberries and display their ip address
-raspberries_ip = $(arp | grep 'b8:27:eb' | awk '{print $1}')
+raspberries_ip=($(arp | grep 'b8:27:eb' | awk '{print $1}'))
 echo $raspberries_ip
