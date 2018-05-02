465012b6951a8eb62f6392b3e8ac48df47167292
Sacha Lhopital <sacha.lhopital@etu.univ-lyon1.fr>
2018-05-02 16:49:42 +0200

    update deployment script

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,4 +1,6 @@
-# Find all Raspberries of the network
-# and display their ip address
-raspberries_ip = ($(arp | grep 'b8:27:eb' | awk '{print $1}'))
+# Ping all devices of the network
+nmap -sn 10.33.120.1-255
+
+# Find all Raspberries and display their ip address
+raspberries_ip = $(arp | grep 'b8:27:eb' | awk '{print $1}')
 echo $raspberries_ip
