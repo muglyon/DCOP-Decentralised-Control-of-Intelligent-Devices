745b2530ad857dfbea4db8fbff6b49e7f26feb53
Sacha Lhopital <sacha.lhopital@etu.univ-lyon1.fr>
2018-05-02 17:18:30 +0200

    Update deployment script

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,6 +1,11 @@
 # Ping all devices of the network
 nmap -sn 10.33.120.1-255
 
-# Find all Raspberries and display their ip address
-raspberries_ip=($(arp | grep 'b8:27:eb' | awk '{print $1}'))
-echo $raspberries_ip
+# Find all Raspberries and send them messages in order to update every one
+raspberries_ip=$(arp | grep 'b8:27:eb' | awk '{print $1}')
+
+for ip in $raspberries_ip
+do
+    mosquitto_pub -h 10.33.120.194 -p 1883 -m "Update yourself please" -t "DCOP/$ip"
+    echo "Update msg sent to DCOP/$ip"
+done
