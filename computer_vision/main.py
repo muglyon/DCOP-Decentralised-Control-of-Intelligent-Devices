7e62ef43017a2136d9b6ff7cc7b864736c7d1864
Sacha Lhopital <sacha.lhopital@etu.univ-lyon1.fr>
2018-06-07 14:31:15 +0200

    Computer_vision without matplotlib

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -1,8 +1,8 @@
 import requests
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from PIL import Image
-from matplotlib import patches
+# from matplotlib import patches
 from io import BytesIO
 
 subscription_key = "7277aec7ea57499f870f860623a285f8"
@@ -26,22 +26,23 @@ response = requests.post(
 faces = response.json()
 
 response = requests.get(image_url)
-image = Image.open(BytesIO(response.content))
-
-plt.figure(figsize=(8, 8))
-ax = plt.imshow(image, alpha=0.6)
-
+# image = Image.open(BytesIO(response.content))
+#
+# # plt.figure(figsize=(8, 8))
+# # ax = plt.imshow(image, alpha=0.6)
+#
 for face in faces:
 
-    fr = face["faceRectangle"]
-    fa = face["faceAttributes"]
-    origin = (fr["left"], fr["top"])
-    p = patches.Rectangle(
-        origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
-    ax.axes.add_patch(p)
-    plt.text(origin[0], origin[1], "%s, %d"%(fa["gender"].capitalize(), fa["age"]),
-             fontsize=20, weight="bold", va="bottom")
-_ = plt.axis("off")
+#     fr = face["faceRectangle"]
+#     fa = face["faceAttributes"]
+#     origin = (fr["left"], fr["top"])
+#     p = patches.Rectangle(
+#         origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
+#     ax.axes.add_patch(p)
+#     plt.text(origin[0], origin[1], "%s, %d"%(fa["gender"].capitalize(), fa["age"]),
+#              fontsize=20, weight="bold", va="bottom")
+# _ = plt.axis("off")
+    print(face["faceAttributes"])
 
-plt.show()
+# plt.show()
 
