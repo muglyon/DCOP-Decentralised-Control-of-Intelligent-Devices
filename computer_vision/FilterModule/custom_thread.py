cf885176c2463e18fcd2d2c99063593954078c4d
root <minis95@hotmail.fr>
2018-06-20 12:09:37 +0200

    WIP Computer_vision

--- computer_vision/FilterModule/custom_thread.py
+++ computer_vision/FilterModule/custom_thread.py
@@ -3,6 +3,7 @@ import time
 import requests
 import json
 import cv2
+import os
 
 from threading import Thread
 from iothub_client import IoTHubMessage
@@ -14,7 +15,7 @@ class MyThread(Thread):
     HUMIDITY = 60
     MSG_TXT = "{\"temperature\": %.2f,\"humidity\": %.2f}"
 
-    IMG_NAME = "temp.jpg"
+    IMG_NAME = 'temp.jpg'
     EMOTION_API = "http://127.0.0.1/image"
 
     def __init__(self, user_context):
@@ -23,21 +24,31 @@ class MyThread(Thread):
 
     def run(self):
 
-        print( "Thread is running !")
-
         while 1:
 
-            cam = cv2.VideoCapture(1)
+            cam = cv2.VideoCapture('/dev/video0')
+
+            if not cam.isOpened():
+                print("[ERR] Cannot open the camera...")
+                print(os.listdir("/dev/"))
+            else:
+                print("[INF] Camera open !")
+
             ret, frame = cam.read()
 
-            cv2.imwrite(IMG_NAME, frame)
-            print("{} written!".format(IMG_NAME))
+            result = cv2.imwrite(self.IMG_NAME, frame)
+            print("{} written!".format(self.IMG_NAME))
+
+            print("cam : ", cam)
+            print("ret, frame : ", ret, " ", frame)
+            print("result : ", result)
+            print(os.listdir("/"))
 
             cam.release()
             cv2.destroyAllWindows()
 
             headers = {'content-type': 'application/octet-stream'}
-            r = requests.post(EMOTION_API, data=open(IMG_NAME, 'rb').read(), headers=headers)
+            r = requests.post(self.EMOTION_API, data=open(self.IMG_NAME, 'rb').read(), headers=headers)
 
             predictions = json.loads(r.content.decode("utf-8"))['predictions']
             
