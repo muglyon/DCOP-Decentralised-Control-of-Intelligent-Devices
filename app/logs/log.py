b41b82ef85ad355fccd56ebf305538bb9edc1d4e
Sacha Lhopital <sacha.lhopital@etu.univ-lyon1.fr>
2018-07-16 16:53:09 +0200

    Remove util matrix of logs

--- app/logs/log.py
+++ app/logs/log.py
@@ -4,6 +4,7 @@ import time
 
 from pythonjsonlogger import jsonlogger
 from logs import elasticsearch
+from logs.message_types import MessageTypes
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 FORMAT_STR = '{"asctime": "%(asctime)s", ' \
@@ -40,7 +41,11 @@ def info(msg, sender_id, msg_type):
     prefix = "" if "DCOP/" in str(sender_id) else "DCOP/"
     payload = json.dumps(msg)
 
-    logger.info(payload, extra={'topic': prefix + str(sender_id), 'type': msg_type})
+    if MessageTypes.UTIL.value in payload:
+        # do not display the matrix in the logs !
+        pass
+    else:
+        logger.info(payload, extra={'topic': prefix + str(sender_id), 'type': msg_type})
 
     # f_read = open(logger.handlers[0].baseFilename, "r")
     # last_line = f_read.readlines()[-1]
